6b2ccdc1527c85197a8986768671f2abee6af2f9
Allen Ji <jixianqia@gmail.com>
2023-05-02 11:56:56 +0800

    Testing rosAria publishing using python

---
+++ pyRosAria.py
@@ -0,0 +1,29 @@
+import rospy
+from geometry_msgs.msg import Twist
+
+def sendCmdVel(linear_velocity, angular_velocity):
+    rospy.init_node('cmd_vel_publisher', anonymous=True)
+    cmd_vel_pub = rospy.Publisher('/RosAria/cmd_vel', Twist, queue_size=10)
+
+    # Create a Twist message to store the linear and angular velocities
+    cmd_vel_msg = Twist()
+    cmd_vel_msg.linear.x = linear_velocity
+    cmd_vel_msg.angular.z = angular_velocity
+
+    # Publish the message
+    cmd_vel_pub.publish(cmd_vel_msg)
+
+    # Log the published velocities
+    rospy.loginfo("Published linear velocity: %s, angular velocity: %s", linear_velocity, angular_velocity)
+
+if __name__ == '__main__':
+    try:
+        # Set desired linear and angular velocities
+        linear_velocity = 0.5  # meters per second
+        angular_velocity = 0.5  # radians per second
+
+        # Call the send_cmd_vel function
+        send_cmd_vel(linear_velocity, angular_velocity)
+
+    except rospy.ROSInterruptException:
+        pass
