5d4fc3221af225937abd75c3d0039bb7c01c1e28
Dhruv <dhruvjobanputra8@gmail.com>
2023-05-16 14:41:49 +0800

    orange detection

--- camera/src/cam.py
+++ camera/src/cam.py
@@ -14,49 +14,42 @@ import numpy as np
 
 rospy.init_node('camera_node')
 
-def camera_example():
-    with OakCamera() as oak:
-        color = oak.create_camera('color')
-        left = oak.create_camera('left')
-        right = oak.create_camera('right')
-        oak.visualize([color, left, right], fps=True)
-        # oak.start(blocking=True)
-        oak.start()
-
-        # # while True:
-        rospy.logerr(dir(color.stream))
-
-
-
-
-def mobile():
-    with OakCamera() as oak:
-        color = oak.create_camera('color')
-        nn = oak.create_nn('yolov4_coco_608x608', color)
-        # nn = oak.create_nn('vehicle-detection-0202', color)
-        oak.visualize([nn], fps=True)
-        oak.start(blocking=True)
-
 def is_appropriate_size(area):
-    if (area > 50) and (area < 10000):
+    if (1000 < area < 50000):
         return True
     else:
         return False
     
 def locate_cone(input_image):
-    RGB_image = input_image
-    HSV_image = cv2.cvtColor(RGB_image, cv2.COLOR_BGR2HSV_FULL)
+    BGR_image = input_image
+
+    HSV_image = cv2.cvtColor(BGR_image, cv2.COLOR_BGR2HSV)
+
+    temp_bgr = np.array([[[10, 10, 180]]], dtype=np.uint8)
+    temp_hsv = cv2.cvtColor(temp_bgr, cv2.COLOR_BGR2HSV)
+
+    # rospy.logerr("HSV")
+    # rospy.logerr(HSV_image[0])
+    # rospy.logerr("BGR")
+    # rospy.logerr(BGR_image[0])
     
-    lower_threshold_orange = np.array([0, 50, 50])
-    upper_threshold_orange = np.array([30, 255, 255])
+    lower_threshold_orange = np.array([0, 50, 0])
+    upper_threshold_orange = np.array([10, 240, 255])
+
+    lower_threshold_orange2 = np.array([170, 50, 0])
+    upper_threshold_orange2 = np.array([180, 240, 255])
     
-    orange_only = cv2.inRange(HSV_image, lower_threshold_orange, upper_threshold_orange) 
+    orange_only = cv2.inRange(HSV_image, lower_threshold_orange, upper_threshold_orange)
+    orange_only2 = cv2.inRange(HSV_image, lower_threshold_orange2, upper_threshold_orange2) 
+
+    orange_only_mask = cv2.bitwise_or(orange_only, orange_only2)
     
-    eroded_orange = cv2.erode((orange_only), cv2.getStructuringElement(1, (11,11)))
+    eroded_orange = cv2.erode((orange_only_mask), cv2.getStructuringElement(1, (11,11)))
     
     eroded_then_dilated_orange = cv2.dilate((eroded_orange), cv2.getStructuringElement(1, (11,11)))
     
-    cv2.imshow("RGB image", RGB_image)
+    cv2.imshow("bgr", BGR_image)
+    # cv2.imshow("eroded_then_dilated_orange", eroded_then_dilated_orange)
     
     totalLabels, label_ids, values, centroids = cv2.connectedComponentsWithStats(eroded_then_dilated_orange, 8, cv2.CV_32S)
     
@@ -71,11 +64,12 @@ def locate_cone(input_image):
     
     for i in labels_of_interest:
         componentMask = (label_ids == i).astype("uint8") * 255
+
         output = cv2.bitwise_or(output, componentMask)
     
     return output, len(labels_of_interest)
 
-def test2():
+def find_cone():
     # Create pipeline
     pipeline = dai.Pipeline()
 
@@ -116,7 +110,7 @@ def test2():
 
 
             __, num_orange = locate_cone(inRgb.getCvFrame())
-            cv2.imshow("rgb", __)
+            cv2.imshow("output", __)
             rospy.logerr(num_orange)
 
             if cv2.waitKey(1) == ord('q'):
@@ -124,6 +118,6 @@ def test2():
 
 
 if __name__ == "__main__":
-    test2()
+    find_cone()
     rospy.spin()
     # rospy.logerr("Hello")
