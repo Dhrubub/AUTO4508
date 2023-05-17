19e7aec38533d4783a2757d32bc131e986f068dd
Dhruv <dhruvjobanputra8@gmail.com>
2023-05-17 11:01:51 +0800

    driving towards the cone

--- camera/src/cam.py
+++ camera/src/cam.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Int32
 
 from depthai_sdk import OakCamera
 
@@ -12,7 +13,19 @@ import numpy as np
 import depthai as dai
 import numpy as np
 
-rospy.init_node('camera_node')
+rospy.init_node('camera_steer')
+
+camera_publisher = rospy.Publisher('/camera_cmd_vel', Twist, queue_size=1)
+cone_publisher = rospy.Publisher('/cone_detected', Bool, queue_size=1)
+
+pose = Twist()
+
+def publish_message(x):
+    # Create an Int32 message and assign the value to publish
+    message = Int32()
+    message.data = x
+
+    # cam_publisher.publish(message)  # Publish the message to the topic
 
 def is_appropriate_size(area):
     if (1000 < area < 50000):
@@ -24,19 +37,11 @@ def locate_cone(input_image):
     BGR_image = input_image
 
     HSV_image = cv2.cvtColor(BGR_image, cv2.COLOR_BGR2HSV)
-
-    temp_bgr = np.array([[[10, 10, 180]]], dtype=np.uint8)
-    temp_hsv = cv2.cvtColor(temp_bgr, cv2.COLOR_BGR2HSV)
-
-    # rospy.logerr("HSV")
-    # rospy.logerr(HSV_image[0])
-    # rospy.logerr("BGR")
-    # rospy.logerr(BGR_image[0])
     
     lower_threshold_orange = np.array([0, 50, 0])
-    upper_threshold_orange = np.array([10, 240, 255])
+    upper_threshold_orange = np.array([1, 240, 255])
 
-    lower_threshold_orange2 = np.array([170, 50, 0])
+    lower_threshold_orange2 = np.array([179, 50, 0])
     upper_threshold_orange2 = np.array([180, 240, 255])
     
     orange_only = cv2.inRange(HSV_image, lower_threshold_orange, upper_threshold_orange)
@@ -48,7 +53,7 @@ def locate_cone(input_image):
     
     eroded_then_dilated_orange = cv2.dilate((eroded_orange), cv2.getStructuringElement(1, (11,11)))
     
-    cv2.imshow("bgr", BGR_image)
+    # cv2.imshow("Colour", BGR_image)
     # cv2.imshow("eroded_then_dilated_orange", eroded_then_dilated_orange)
     
     totalLabels, label_ids, values, centroids = cv2.connectedComponentsWithStats(eroded_then_dilated_orange, 8, cv2.CV_32S)
@@ -59,6 +64,49 @@ def locate_cone(input_image):
         area = values[i, cv2.CC_STAT_AREA]
         if is_appropriate_size(area) == True:
             labels_of_interest.append(i)
+            
+    centroid_x = -1
+            
+    if len(labels_of_interest) > 1:
+            
+        area_array = np.zeros((1,len(labels_of_interest)))
+        
+        for i in range(len(labels_of_interest)):
+            # rospy.logerr("height: {}".format(len(area_array)))
+            # rospy.logerr("width: {}".format(len(area_array[0])))
+            area_array[0][i] = values[labels_of_interest[i], cv2.CC_STAT_AREA]
+            
+        index_of_largest_cluster = np.argmax(area_array)
+        
+        # rospy.logerr(index_of_largest_cluster)
+        # rospy.logerr(len(labels_of_interest))
+        
+        cetroid_of_largest_orange = centroids[labels_of_interest[index_of_largest_cluster]]
+        
+        # rospy.logerr(cetroid_of_largest_orange)
+        
+        centroid_x = centroids[labels_of_interest[index_of_largest_cluster]][0]
+        
+        if (100 > centroid_x > 0):
+            pose.angular.z = 0.3
+            camera_publisher.publish(pose)
+            rospy.logerr("go left")
+            
+        if (255 > centroid_x > 155):
+            pose.angular.z = -0.3
+            camera_publisher.publish(pose)
+            rospy.logerr("go right")
+            
+        if (centroid_x > 100) and (centroid_x < 155):
+            pose.angular.z = 0
+            camera_publisher.publish(pose)
+            rospy.logerr("go straight")
+            
+        if (centroid_x == -1):
+            pose.angular_z = 0
+            camera_publisher.publish(pose)
+            rospy.logerr("No orange object detected")
+        
         
     output = np.zeros(eroded_then_dilated_orange.shape, dtype="uint8")
     
@@ -67,7 +115,7 @@ def locate_cone(input_image):
 
         output = cv2.bitwise_or(output, componentMask)
     
-    return output, len(labels_of_interest)
+    return output, len(labels_of_interest), centroid_x
 
 def find_cone():
     # Create pipeline
@@ -105,13 +153,19 @@ def find_cone():
         while True:
             inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
 
-            # Retrieve 'bgr' (opencv format) frame
-            #cv2.imshow("rgb", inRgb.getCvFrame())
+            #Retrieve 'bgr' (opencv format) frame
+            # cv2.imshow("rgb", inRgb.getCvFrame())
 
-
-            __, num_orange = locate_cone(inRgb.getCvFrame())
-            cv2.imshow("output", __)
-            rospy.logerr(num_orange)
+            output, num_orange, orange_x = locate_cone(inRgb.getCvFrame())
+        
+            # cv2.imshow("output", output)
+            
+            #rospy.logerr(num_orange)
+            msg = Bool()
+            msg.data = num_orange > 0
+            cone_publisher.publish(msg)
+            
+            # publish_message(orange_x)
 
             if cv2.waitKey(1) == ord('q'):
                 break
@@ -120,4 +174,3 @@ def find_cone():
 if __name__ == "__main__":
     find_cone()
     rospy.spin()
-    # rospy.logerr("Hello")
--- master/src/drive.py
+++ master/src/drive.py
@@ -2,25 +2,36 @@
 import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Bool
+from enum import Enum
 
 
 rospy.init_node('master')
 
+class CurrentState(Enum):
+    MANUAL = "MANUAL"
+    AUTO = "AUTO"
+    CONE_FOLLOW = "CONE_FOLLOW"
+    SCAN = "SCAN"
+
 class State:
     def __init__(self):
-        self.is_manual = True
         self.deadman = False
         self.pose = Twist()
 
-        # additional not used currently
-        self.is_scanning = False
+        self.current_state = CurrentState.MANUAL
+
 
     def set_manual(self, data):
-        self.is_manual = data
+        if data:
+            self.current_state = CurrentState.MANUAL
+        else:
+            self.current_state = CurrentState.AUTO
+
+    def set_state(self, data):
+        self.current_state = data
 
     def set_scan(self):
         self.pose = Twist()
-        self.is_scanning = True
 
     def set_pose(self, p):
         self.pose = p
@@ -32,23 +43,52 @@ state = State()
 rosaria_cmd_vel_publisher = rospy.Publisher('/RosAria/cmd_vel', Twist, queue_size=1)
 
 def man_cmd_vel(data):
-    if state.is_manual:
+    if state.current_state == CurrentState.MANUAL:
         state.set_pose(data)
 
 def auto_cmd_vel(data):
-    if not state.is_manual:
+    if state.current_state == CurrentState.AUTO:
         if state.deadman:
             state.set_pose(data)
         else:
             state.set_pose(Twist())
 
 def manual_toggle(data):
-    if not state.is_manual == data.data:
+    # Set to manual if not manual
+    if data.data and not state.current_state == CurrentState.MANUAL:
         state.set_pose(Twist())
-        state.set_manual(data.data)
+        state.set_state(CurrentState.MANUAL)
+
+    # Set to auto if manual
+    elif not data.data and state.current_state == CurrentState.MANUAL:
+        state.set_pose(Twist())
+        state.set_state(CurrentState.AUTO)
 
 def deadman_callback(data):
     state.deadman = data.data
+
+def cone_detected(data):
+    if not state.current_state == CurrentState.CONE_FOLLOW and not state.current_state == CurrentState.MANUAL:
+        if data.data == True:
+        # rospy.logerr(data.data)
+        
+            state.set_state(CurrentState.CONE_FOLLOW)
+            pose = Twist()
+            pose.linear.x = 0.3
+            state.set_pose(pose)
+        # rospy.logerr(state.current_state)
+        elif data.data == False:
+            state.set_state(CurrentState.AUTO)
+            # rospy.logerr(state.current_state)
+
+def camera_cmd_vel(data):
+    if state.current_state == CurrentState.CONE_FOLLOW:
+        pose = Twist()
+        pose.angular.z = data.angular.z
+        pose.linear.x = 0.3
+        state.set_pose(pose)
+    # rospy.logerr(state.current_state)
+
 
 if __name__ == "__main__":
     rate = rospy.Rate(50)
@@ -58,8 +98,13 @@ if __name__ == "__main__":
     rospy.Subscriber('/man_cmd_vel', Twist, man_cmd_vel)
     rospy.Subscriber('/manual_toggle', Bool, manual_toggle)
     rospy.Subscriber('/deadman_state', Bool, deadman_callback)
+    rospy.Subscriber('/cone_detected', Bool, cone_detected)
+    rospy.Subscriber('/camera_cmd_vel', Twist, camera_cmd_vel)
+
 
     while not rospy.is_shutdown():
-        rosaria_cmd_vel_publisher.publish(state.pose)
-        # rospy.logerr(state.pose.linear.x)
+        if not state.current_state == CurrentState.MANUAL and state.deadman or state.current_state == CurrentState.MANUAL:
+            rosaria_cmd_vel_publisher.publish(state.pose)
+        rospy.logerr(state.current_state)
+
         rate.sleep()
